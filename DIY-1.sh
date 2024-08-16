46cbbf627749131050337f9e371d1a9386504085
minyangout <minyang3194@outlook.com>
2024-08-16 11:38:39 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -8,8 +8,13 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 2-添加 PowerOff 关机插件
 git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 opentomcat 主题
+# 3-添加主题
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
 # 4-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
