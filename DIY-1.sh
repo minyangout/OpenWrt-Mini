7dff51ab235bb02503eeeedca7e48e2d0be358fe
minyangout <minyang3194@outlook.com>
2024-08-18 21:00:24 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -16,18 +16,17 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # 6-添加额外插件
-git clone -b js --single-branch https://github.com/gngpp/luci-theme-design package/luci-theme-design
+git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design.git package/luci-theme-design
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata.git package/v2ray-geodat
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-pushbot
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd luci-lib-xterm taskd
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-linkease linkease ffmpeg-remux
-git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package luci-app-autotimeset
-git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package luci-app-advanced
-git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package luci-app-socat
-git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package luci-app-fileassistant
-
+git clone https://github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-autotimeset.git package/luci-app-autotimeset
+git clone https://github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-advanced.git package/luci-app-advanced
+git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-socat.git package/luci-app-socat
+git clone https://github.com/sirpdboy/sirpdboy-package/tree/main/luci-app-fileassistant.git package/luci-app-fileassistant
 # 添加 diskman 插件
 sed -i '$a\src-git diskman https://github.com/lisaac/luci-app-diskman' ./feeds.conf.default
 
